949852583ecbd9b925786ed708efdaa9e0a976a5
mccalluc <mccalluc@users.noreply.github.com>
2020-02-26 12:28:28 -0500

    black

--- tests/test_directory_schema.py
+++ tests/test_directory_schema.py
@@ -6,12 +6,15 @@ from directory_schema import directory_schema
 def test_directory_schema():
     assert directory_schema is not None
 
+
 def test_dir_to_dict():
-    fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures', 'fake-directory')
+    fixture_path = os.path.join(os.path.dirname(__file__), "fixtures", "fake-directory")
     assert directory_schema.dir_to_dict(fixture_path) == [
-        {'type': 'file', 'name': 'README.md'},
-        {'type': 'file', 'name': '.dot-files-should-be-listed'},
-        {'type': 'directory', 'name': 'src', 'contents':[
-            {'type': 'file', 'name': 'fake.py'}
-        ]}
+        {"type": "file", "name": "README.md"},
+        {"type": "file", "name": ".dot-files-should-be-listed"},
+        {
+            "type": "directory",
+            "name": "src",
+            "contents": [{"type": "file", "name": "fake.py"}],
+        },
     ]
