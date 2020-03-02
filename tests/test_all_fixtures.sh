27d0d8002d458b872d4977b6ee18f166611d09c3
mccalluc <mccalluc@users.noreply.github.com>
2020-03-02 11:49:54 -0500

    More colors to distinguish between positive and negative cases

--- tests/test_all_fixtures.sh
+++ tests/test_all_fixtures.sh
@@ -4,6 +4,7 @@ set -o pipefail
 
 red=`tput setaf 1`
 green=`tput setaf 2`
+cyan=`tput setaf 6`
 reset=`tput sgr0`
 die() { set +v; echo "$red$*$reset" 1>&2 ; exit 1; }
 
@@ -15,10 +16,10 @@ for SCHEMA in *.*; do
     DIR_STEM=$(basename $DIR)
     CMD="directory_schema $DIR $SCHEMA"
     if [ $SCHEMA_STEM == $DIR_STEM ]; then
-      echo "${green}Expect '$CMD' to pass...${reset}"
+      echo "${cyan}Expect '$CMD' to ${green}pass...${reset}"
       $CMD > /dev/null || die "'$CMD' failed, and it should have passed."
     else
-      echo "${green}Expect '$CMD' to fail...${reset}"
+      echo "${cyan}Expect '$CMD' to ${red}fail...${reset}"
       ! $CMD > /dev/null || die "'$CMD' passed, and it should have failed."
       # TODO: This does not distinguish between the failures we want,
       # and python errors with tracebacks.
